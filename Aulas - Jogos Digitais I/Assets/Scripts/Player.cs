0fba57ffb915172323b6cad5b145ad031720ba87
Renzo Nogaredi <149898593+renzonogar@users.noreply.github.com>
2025-09-09 15:39:42 -0300

    Movimentação & Pulo

--- Aulas - Jogos Digitais I/Assets/Scripts/Player.cs
+++ Aulas - Jogos Digitais I/Assets/Scripts/Player.cs
@@ -2,15 +2,67 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Animator anim;
+    private Rigidbody2D rigd;
+    public float movespeed;
+
+    public float jumpForce = 5f;
+    public bool isGround;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        anim = GetComponent<Animator>();
+        rigd = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float teclas = Input.GetAxis("Horizontal");
+        rigd.linearVelocity = new Vector2(teclas * movespeed, rigd.linearVelocity.y);
+
+        if (teclas > 0 && isGround == true)
+        {   // Andar pra direita
+            transform.eulerAngles = new Vector2(0, 0);
+            anim.SetInteger("transition", 1);
+        }
+
+        if (teclas < 0 && isGround == true)
+        {   // Andar pra esquerda
+            transform.eulerAngles = new Vector2(0, 180);
+            anim.SetInteger("transition", 1);
+        }
+
+        if (teclas == 0 && isGround == true)
+        {   // Ficar parado
+            anim.SetInteger("transition", 0);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            rigd.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetInteger("transition", 2);
+            isGround = false;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "tagGround")
+        {
+            isGround = true;
+            Debug.Log("ta no chao");
+        }
         
     }
 }
